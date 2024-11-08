82b1875d955fb5934827fb73a6e96523ffbb2a84
dfcb2012 <dfcb2014@gmail.com>
2024-11-08 23:03:03 +0000

    Completed exercises 01, 02 and 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (string, number) {
+  if (number < 0) return "ERROR";
+  let str = "";
+  for (let i = 0; i < number; i++) {
+    str += string;
+  }
+  return str;
 };
 
 // Do not edit below this line

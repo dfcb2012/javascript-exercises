2051b2ccc614b6fd1f6a795a150650f0658c5c60
dfcb2012 <dfcb2014@gmail.com>
2024-11-28 00:10:45 +0000

    Completed exercises from 08 to 10

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function (x, y) {
+  if (x >= 0 && y >= 0) {
+    return x + y;
+  }
+  return false;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((total, currentItem) => total + currentItem, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((total, currentItem) => total * currentItem, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  if (b < 0) return false;
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  return n === 0 ? 1 : n * factorial(n - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +33,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const cleanStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  const reversedStr = cleanStr.split("").reverse().join("");
+  return cleanStr === reversedStr;
 };
 
 // Do not edit below this line

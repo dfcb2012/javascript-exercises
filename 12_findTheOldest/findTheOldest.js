32c317ebb99b1e6d4edad3ecb38c3ece300fff78
dfcb2012 <dfcb2014@gmail.com>
2024-11-28 22:29:22 +0000

    Complete exercises 11 and 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const currentYear = new Date().getFullYear();
 
+  return people.reduce((oldestPerson, person) => {
+    const personYear = person.yearOfDeath || currentYear;
+    const personAge = personYear - person.yearOfBirth;
+
+    const oldestPersonYear = oldestPerson.yearOfDeath || currentYear;
+    const oldestPersonAge = oldestPersonYear - oldestPerson.yearOfBirth;
+
+    return personAge > oldestPersonAge ? person : oldestPerson;
+  });
 };
 
 // Do not edit below this line

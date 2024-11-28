701e7a268815db1c6deaf09fd4719b09f27a22f5
dfcb2012 <dfcb2014@gmail.com>
2024-11-28 00:10:06 +0000

    Completed exercises from 08 to 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  const n = parseInt(num, 10);
 
+  if (n < 0 || isNaN(n)) return "OOPS";
+  if (n <= 1) return n;
+
+  currValue = 1;
+  prevValue = 0;
+
+  for (let i = 2; i <= n; i++) {
+    let next = currValue + prevValue;
+    prevValue = currValue;
+    currValue = next;
+  }
+  return currValue;
 };
 
 // Do not edit below this line
